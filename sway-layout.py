8524668c501efa4105ecdd17b60a03db62ae3ac1
Jeremy Symon <jtsymon@gmail.com>
2019-03-31 16:23:22 +1300

    Change config format and remove need for synch

--- sway-layout.py
+++ sway-layout.py
@@ -8,13 +8,14 @@ import yaml
 import sys
 import os
 
-processes = []
+programs = []
 
 parser = argparse.ArgumentParser(description='Start some applications with a configured layout')
 parser.add_argument('-t', '--timeout', help='How long to wait for applications to start', type=int, default=5)
 parser.add_argument('-v', '--verbose', help='Log actions as they are performed', action='store_true')
-parser.add_argument('-s', '--synch', help='Make sure to start applications on their correct workspace, by waiting for each application to start. Helps if they have problems with being resized', action='store_true')
-parser.add_argument('file', help='YAML file defining applications to start (or pipe to STDIN)', nargs=(1 if sys.stdin.isatty() else '?'))
+parser.add_argument('file',
+                    help='YAML file defining applications to start (or pipe to STDIN)',
+                    nargs=(1 if sys.stdin.isatty() else '?'))
 args = parser.parse_args()
 
 def find_process(cmdline):
@@ -22,35 +23,33 @@ def find_process(cmdline):
     if args.verbose:
         printable = ' '.join(clargs)
         print(f"Matching {printable}")
-    for proc in processes:
+    for proc in programs:
         if clargs == proc['match']:
             return proc
 
 def on_window_new(i3, e):
     container_id = e.container.id
     process_id = e.container.pid
+    if args.verbose:
+        print(f'on_window_new {container_id}:{process_id}')
     with open(f'/proc/{process_id}/cmdline') as f:
         cmdline = f.read().strip(' \t\r\n\0')
     proc = find_process(cmdline)
     if proc:
-        processes.remove(proc)
+        programs.remove(proc)
         if args.verbose:
             printable = ' '.join(proc['cmdline'])
             print(f'Positioning {printable}')
         if 'workspace' in proc:
             workspace = proc['workspace']
             e.container.command(f'move container to workspace {workspace}')
-            if 'output' in proc:
-                output = proc['output']
-                i3.command(f'[workspace={workspace}] move workspace to output {output}')
-    if not processes:
+    if not programs:
         i3.main_quit()
 
 i3 = i3ipc.Connection()
 i3.on('window::new', on_window_new)
 
 def launch(proc):
-    proc['cmdline'] = list(str(x) for x in proc['cmdline'])
     if args.verbose:
         printable = ' '.join(proc['cmdline'])
         print(f'Starting {printable}')
@@ -58,8 +57,9 @@ def launch(proc):
     if 'env' in proc:
         env = dict(os.environ)
         env.update(proc['env'])
-    if not 'match' in proc:
-        proc['match'] = proc['cmdline']
+    if args.verbose:
+        printable = ' '.join(proc['match'])
+        print(f'Match {printable}')
     if 'workspace' in proc:
         workspace = proc['workspace']
         i3.command(f'workspace {workspace}')
@@ -67,6 +67,32 @@ def launch(proc):
 
 def run():
     i3.main(args.timeout)
+
+def format_cmdline(cmdline):
+    if isinstance(cmdline, list):
+        return list(str(x) for x in cmdline)
+    return [str(cmdline)]
+
+def parse_config(config):
+    programs = []
+    for output, workspaces in config.items():
+        for workspace, windows in workspaces.items():
+            for title, config in windows.items():
+                program = {
+                    'workspace': workspace
+                }
+                if config:
+                    program['cmdline'] = config['cmdline'] if 'cmdline' in config else title
+                    program['match'] = config['match'] if 'match' in config else program['cmdline']
+                    if 'env' in config:
+                        program['env'] = config['env']
+                else:
+                    program['cmdline'] = title
+                    program['match'] = title
+                program['cmdline'] = format_cmdline(program['cmdline'])
+                program['match'] = format_cmdline(program['match'])
+                programs.append(program)
+    return programs
 
 if args.file:
     with open(args.file[0]) as f:
@@ -76,17 +102,20 @@ else:
 if args.verbose:
     print(f'Loaded config: {config}')
 
-if args.synch:
-    for proc in config:
-        processes = [proc]
-        t = threading.Thread(target=run)
-        t.start()
-        launch(proc)
-        t.join()
-else:
-    processes = config
-    t = threading.Thread(target=run)
-    t.start()
-    for proc in config:
-        launch(proc)
-    t.join()
+programs = parse_config(config)
+if args.verbose:
+    print(f'Parsed windows: {programs}')
+
+# Start programs
+t = threading.Thread(target=run)
+t.start()
+
+for program in programs:
+    launch(program)
+
+t.join()
+
+# Move workspaces to correct outputs
+for output, workspaces in config.items():
+    for workspace in workspaces:
+        i3.command(f'[workspace={workspace}] move workspace to output {output}')
